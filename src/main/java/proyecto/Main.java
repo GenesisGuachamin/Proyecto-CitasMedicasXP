0eb348822077dc50e1e77027489badec62dc9583
Enfis123 <toncenose123@gmail.com>
2023-06-26 18:14:42 -0500

    Registrar Cita

--- src/main/java/proyecto/Main.java
+++ src/main/java/proyecto/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
+        //Probando
         public static void main(String[] args) {
             DatabaseConnection databaseConnection = new DatabaseConnection();
             databaseConnection.conectar();
